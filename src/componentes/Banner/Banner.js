98b024356ada3b46deaf22ad59523d86502565a5
Jeferson Alves <jefersonalvessantos62@gmail.com>
2026-03-16 22:51:47 -0300

    feat: Primeiro componente: Banner, adicionando imagens.

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import "./Banner.css";
+function Banner() {
+  return (
+    //JSX
+    <header className="banner">
+      <img src="/img/banner.png" alt="Banner principal da página do Organo." />
+    </header>
+  );
+}
+
+export default Banner;
