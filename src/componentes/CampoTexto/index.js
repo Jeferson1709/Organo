4999c9951dd3ea15fb6a59cd537f8d809bd51f94
Jeferson Alves <jefersonalvessantos62@gmail.com>
2026-03-18 23:38:19 -0300

    feat: Lista dos times

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,12 +1,12 @@
-import './CampoTexto.css'
-const CampoTexto = (props) =>{
-     const placeholderModificada =`${props.placeholder}...`
-    return(
-        <div className="campo-texto">
-            <label>{props.label}</label>
-            <input placeholder={placeholderModificada}/>
-        </div>
-    )
-}
+import "./CampoTexto.css";
+const CampoTexto = (props) => {
+  const placeholderModificada = `${props.placeholder}...`;
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input placeholder={placeholderModificada} />
+    </div>
+  );
+};
 
-export default CampoTexto
+export default CampoTexto;
