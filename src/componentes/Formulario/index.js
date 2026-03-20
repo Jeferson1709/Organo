4650edc6ecc390d8e81c06a139982eb74d5167cb
Jeferson Alves <jefersonalvessantos62@gmail.com>
2026-03-19 23:43:08 -0300

    feat: Criando o botão

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,16 +1,17 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 const Formulario = () => {
-  const times =[
-    'Programação',
-    'Front-End',
-    'Data Science',
-    'Devops', 
-    'UX e Design', 
-    'Mobile', 
-    'Inovação e Gestão'
-  ]
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
   return (
     <section className="formulario">
       <form>
@@ -18,7 +19,8 @@ const Formulario = () => {
         <CampoTexto label="Nome" placeholder="Digite o seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite o seu Cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa label='Time' itens={times}/>
+        <ListaSuspensa label="Time" itens={times} />
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
