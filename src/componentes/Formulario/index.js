953be4918171b33c429be642212df255fdafc3b5
Jeferson Alves <jefersonalvessantos62@gmail.com>
2026-03-20 23:25:44 -0300

    feat: Escutando eventos em React.

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -12,14 +12,20 @@ const Formulario = () => {
     "Mobile",
     "Inovação e Gestão",
   ];
+
+  const aoSalvar = (event) => {
+    event.preventDefault();
+    console.log("O form foi submetido");
+  };
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador </h2>
-        <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite o seu Cargo" />
+        <CampoTexto obrigatorio = {true} label="Nome" placeholder="Digite o seu nome" />
+        <CampoTexto  obrigatorio = {true} label="Cargo" placeholder="Digite o seu Cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times} />
+        <ListaSuspensa obrigatorio = {true}  label="Time" itens={times} />
         <Botao>Criar Card</Botao>
       </form>
     </section>
