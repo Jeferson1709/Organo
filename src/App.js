4801164f70521aa13284d8b5aac50dcde7a54c46
Jeferson Alves <jefersonalvessantos62@gmail.com>
2026-03-23 23:08:02 -0300

    feat:Criando o componente Time

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
+import Time from "./componentes/Time";
 function App() {
-  const [calaboradores , setCalaboradores] = useState('');
+  const [calaboradores, setCalaboradores] = useState("");
   const aoNovoCalaboradorAdicionado = (calaborador) => {
     console.log(calaborador);
-    setCalaboradores([...calaboradores , calaborador])
-  }
+    setCalaboradores([...calaboradores, calaborador]);
+  };
   return (
     <div className="App">
       <Banner />
-      <Formulario 
-      aoColaboradorCadastrado={colaborador => aoNovoCalaboradorAdicionado(colaborador)}
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoCalaboradorAdicionado(colaborador)
+        }
       />
+      <Time nome="Programção" />
+      <Time nome="Front-End" />
+      <Time nome="Data Science" />
     </div>
   );
 }
