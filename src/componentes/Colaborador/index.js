5076806c5c1333b9b38d1f2a461dc5cf51e4f7ff
Jeferson Alves <jefersonalvessantos62@gmail.com>
2026-03-30 23:38:51 -0300

    feat: Card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,16 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img src="https://github.com/Jeferson1709.png" alt="Jeferson Alves" />
+      </div>
+      <div className="rodape">
+        <h4>Jeferson</h4>
+        <h5> Estudante</h5>
+      </div>
+    </div>
+  );
+};
+export default Colaborador;
